c88f909881dff012a2cf22ed675ab68d71351540
navisingh14 <navjotgogna@gmail.com>
2017-04-14 00:16:52 -0400

    adding and deleting rfcs and clients on closing connection

--- Client.py
+++ Client.py
@@ -3,16 +3,14 @@ import random
 import pickle
 
 serverName = '127.0.0.1'
-serverPort = 7754
+serverPort = 7765
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((serverName, serverPort))
 
 port = random.randint(8000,9000)
-rfc_num = 4
-rfc_title = 'hello'
+rfc_num = [4]
+rfc_title = ['hello']
 data= pickle.dumps([port,rfc_num, rfc_title])
 
 clientSocket.send(data)
-modifiedSentence = clientSocket.recv(1024)
-print 'From Server:', modifiedSentence
 clientSocket.close()
--- Server.py
+++ Server.py
@@ -6,7 +6,7 @@ import threading
 import thread
 import pickle
 
-serverPort = 7754
+serverPort = 7765
 serverSocket = socket(AF_INET, SOCK_STREAM)
 serverSocket.bind(('',serverPort))
 serverSocket.listen(1)
@@ -21,15 +21,29 @@ def add_client_to_active_peers(peer_name, upload_port):
     activePeers[peer_name] = upload_port
 
 def add_to_rfc_list(rfc_num, rfc_title, peer_name):
-    if rfc_num in RFC:
-        RFC[rfc_num][1].append(peer_name)
-    else:
-        RFC[rfc_num] = [rfc_title,[peer_name]]
+    for i in range(len(rfc_num)):
+        if rfc_num[i] in RFC:
+            RFC[rfc_num[i]][1].append(peer_name)
+        else:
+            RFC[rfc_num[i]] = [rfc_title[i],[peer_name]]
+
+
+
+def del_client_from_active_peers(peer_name):
+    global activePeers
+    del(activePeers[peer_name])
+
+def del_from_rfc_list(rfc_num,peer_name):
+    global RFC
+    for i in range(len(rfc_num)):
+        RFC[rfc_num[i]][1].remove(peer_name)
+
 
 
 def new_client_thread(connSock, addr):
     global activePeers, RFC
-    data = pickle.loads(connectionSocket.recv(1024))
+    data = pickle.loads(connSock.recv(4096))
+    print data
     upload_port = data[0]
     rfc_num = data[1]
     rfc_title = data[2]
@@ -39,6 +53,15 @@ def new_client_thread(connSock, addr):
     print activePeers
     print RFC
     print '\n'
+    while True:
+        print len(connSock.recv(1024))
+        if(len(connSock.recv(1024)) == 0):
+            del_client_from_active_peers(addr[0])
+            del_from_rfc_list(rfc_num, addr[0])
+            print activePeers
+            print RFC
+            print 'closed'
+            break
     connectionSocket.close()
     
 
@@ -46,7 +69,5 @@ def new_client_thread(connSock, addr):
 
 #Master Socket listening forever
 while 1:
-    print activePeers
-    print RFC
     connectionSocket, addr = serverSocket.accept()
     thread.start_new_thread(new_client_thread, (connectionSocket,addr))
