5b18589d0e94420596a355276220455c8017889e
navisingh14 <navjotgogna@gmail.com>
2017-04-15 20:30:21 -0400

    basic framework of peer2server protocol

--- Client.py
+++ Client.py
@@ -1,16 +1,90 @@
-from socket import *
+import socket
 import random
+import thread
 import pickle
 
-serverName = '127.0.0.1'
-serverPort = 7765
-clientSocket = socket(AF_INET, SOCK_STREAM)
-clientSocket.connect((serverName, serverPort))
+#Setting up UploadServer
 
-port = random.randint(8000,9000)
-rfc_num = [4]
-rfc_title = ['hello']
-data= pickle.dumps([port,rfc_num, rfc_title])
 
-clientSocket.send(data)
-clientSocket.close()
+
+def setupUploadServer(port):
+    uploadSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    uploadSocket.bind(('',port))
+    uploadSocket.listen(5)
+    while True:
+        connectionSocket, addr = uploadSocket.accept()
+        RFC = connectionSocket.recv(4096)
+        #get the data from the rfc
+        #connectionSocket.send(data)
+        
+    
+
+def getUploadSocket():
+    port = random.randint(7000,8000)
+    return port
+
+
+def add():
+    request_body = "ADD RFC " + str(rfc_num)+" P2P-CI/1.0 \n"\
+              "Host: " + str(host)+"\n"\
+              "Port: " + str(port)+"\n"\
+              "Title: " + str(rfc_title)+"\n"
+    return request_body
+
+def lookup(rfc_num, rfc_title):
+    request_body = "LOOKUP RFC " + str(rfc_num)+" P2P-CI/1.0 \n"\
+              "Host: " + str(host)+"\n"\
+              "Port: " + str(port)+"\n"\
+              "Title: " + str(rfc_title)+"\n"
+    return request_body
+
+def list_rfc():
+    request_body = "LIST ALL P2P-CI/1.0 \n"\
+              "Host: "+str(host)+"\n"\
+              "Port: "+str(port)+"\n"
+    return request_body
+
+
+#Contacting Server
+def ContactServer(port, clientSocket):
+    user_input = raw_input("> Enter the command: ADD, LIST, LOOKUP, GET, EXIT")
+    if user_input == 'ADD':
+        data = pickle.dumps(list_rfc())
+        clientSocket.send(data)
+        ContactServer(port, clientSocket)
+    elif user_input == 'LIST':
+        data = pickle.dumps(list_rfc())
+        clientSocket.send(data)
+        ContactServer(port, clientSocket)
+    elif user_input == 'LOOKUP':
+        rfc_num = input("Enter the RFC number: ")
+        rfc_title = raw_input("Enter the RFC title: ")
+        data = pickle.dumps(lookup(rfc_num, rfc_title))
+        clientSocket.send(data)
+        ContactServer(port, clientSocket)
+    elif user_input == 'GET':
+        ContactServer(port, clientSocket)
+    elif user_input == 'EXIT':
+        clientSocket.close()
+'''    rfc_num = [4]
+    rfc_title = ['hello']
+    data= pickle.dumps([port,rfc_num, rfc_title]) 
+    clientSocket.send(data)
+    received_data = clientSocket.recv(4096)
+    print received_data'''
+
+
+print socket.gethostname();
+
+
+if __name__ == '__main__':
+    port = getUploadSocket()
+    print port
+    thread.start_new_thread(setupUploadServer, (port,))
+    serverName = '127.0.0.1'
+    serverPort = 7767
+    clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    host = socket.gethostname()
+    clientSocket.connect((serverName, serverPort))
+    ContactServer(port, clientSocket)
+ 
--- Server.py
+++ Server.py
@@ -6,14 +6,16 @@ import threading
 import thread
 import pickle
 
-serverPort = 7765
+serverPort = 7767
 serverSocket = socket(AF_INET, SOCK_STREAM)
 serverSocket.bind(('',serverPort))
 serverSocket.listen(1)
 print 'the server is ready to receive'
 
-
+#peer_name and upload_port
 activePeers = {}
+
+#rfc_num, rfc_title, peer_name
 RFC = {}
 
 
@@ -42,9 +44,24 @@ def del_from_rfc_list(rfc_num,peer_name):
 
 def new_client_thread(connSock, addr):
     global activePeers, RFC
-    data = pickle.loads(connSock.recv(4096))
-    print data
-    upload_port = data[0]
+    try:
+        while True:
+            data = pickle.loads(connSock.recv(4096))
+            print data
+            print len(data)
+            if(len(data) == 0):
+                del_client_from_active_peers(addr[0])
+                del_from_rfc_list(rfc_num, addr[0])
+                print activePeers
+                print RFC
+                print 'closed'
+                break
+        connectionSocket.close()
+    except EOFError:
+        print 'closed'
+        connectionSocket.close()
+
+''' upload_port = data[0]
     rfc_num = data[1]
     rfc_title = data[2]
     add_client_to_active_peers(addr[0], upload_port)
@@ -53,16 +70,7 @@ def new_client_thread(connSock, addr):
     print activePeers
     print RFC
     print '\n'
-    while True:
-        print len(connSock.recv(1024))
-        if(len(connSock.recv(1024)) == 0):
-            del_client_from_active_peers(addr[0])
-            del_from_rfc_list(rfc_num, addr[0])
-            print activePeers
-            print RFC
-            print 'closed'
-            break
-    connectionSocket.close()
+    '''
     
 
 
